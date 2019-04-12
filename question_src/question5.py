ad140de89022b931d74077fc5f93f15eb79b4712
bskwon0526 <bskwon0526@gmail.com>
2019-04-12 17:16:02 +0900

    2019-04-12

---
+++ question_src/question5.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+c = input()
+if(c == 'y'):
+    print("놀이기구에 탑승하세요")
+else:
+    if(a >= 12 or b >=130):
+        print("놀이기구에 탑승하세요")
+    else:
+        print("놀이기구에 탑승할 수 없습니다")
