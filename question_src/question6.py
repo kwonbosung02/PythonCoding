e499ba823b77969c9f92291acf42f3a35cffc95d
bskwon0526 <bskwon0526@gmail.com>
2019-04-15 14:09:22 +0900

    2019-04-15

--- question_src/question6.py
+++ question_src/question6.py
@@ -6,7 +6,14 @@ Matrix = [[0]*19 for i in range(19)]
 cnt = 0
 for i in range(get_num):
     array.extend(input().replace("[","").replace("]","").split(','))
+
     array = [int (i) for i in array]
-for i in range(get_num):
-    Matrix[array[i],array[i+1]] = 1
 print(array)
+
+for i in range(get_num):
+    Matrix[array[2*i]][array[2*i + 1]] = 1
+    
+for i in range(19):
+    print(str(Matrix[i]).replace("[","").replace("]",""))
+
+        
